b0cc4ec2fe07137f9d5ca811b1d387dfa717f2d8
Andrew Tai <andrewtai.ye@gmail.com>
2022-10-03 14:01:27 +0800

    Transferred Data Fetch to App.js. Split fetch into initial and main

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // <------------ React Components ------------>
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 
 // <------------ Components ------------>
@@ -14,11 +14,83 @@ import "./App.css";
 
 // <------------ Main App ------------>
 export default function App() {
+  const [isFetchingData, setIsFetchingData] = useState(false);
+  const [dataSet, setDataSet] = useState([]);
+  const [displaySet, setDisplaySet] = useState([]);
+
+  // <------------ Get display set from Firebase ------------>
+  const fetchDisplaySet = async (url) => {
+    setIsFetchingData(true);
+    console.log("Fetching Initial Display Data");
+
+    try {
+      const res = await fetch(url);
+
+      if (res.status !== 200) {
+        throw new Error("Something went wrong.");
+      }
+
+      const data = await res.json();
+      const dataArray = Object.keys(data).map((key) => data[key]);
+
+      console.log(dataArray);
+      setDisplaySet(dataArray);
+    } catch (error) {
+      console.log(error.message);
+    }
+
+    setIsFetchingData(false);
+    console.log("Finished Fetching Initial Display Data");
+  };
+  // <------------ End of get data from Firebase ------------>
+
+  // <------------ Get data set from Firebase ------------>
+  const fetchDataSet = async (url) => {
+    setIsFetchingData(true);
+    console.log("Fetching Main Data Set");
+
+    try {
+      const res = await fetch(url);
+
+      if (res.status !== 200) {
+        throw new Error("Something went wrong.");
+      }
+
+      const data = await res.json();
+
+      console.log(data);
+      setDisplaySet(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+
+    setIsFetchingData(false);
+    console.log("Finished Fetching Main Data Set");
+  };
+  // <------------ End of get data from Firebase ------------>
+
+  useEffect(() => {
+    let url = `https://andrewtai-school-project-default-rtdb.asia-southeast1.firebasedatabase.app/.json?orderBy="id"&limitToFirst=12`;
+    fetchDisplaySet(url);
+
+    url = `https://andrewtai-school-project-default-rtdb.asia-southeast1.firebasedatabase.app/.json`;
+    fetchDataSet(url);
+  }, []);
+
   return (
     <div>
       <Routes>
         <Route path="/" element={<Splash />} />
-        <Route path="/home" element={<Home />} />
+        <Route
+          path="/home"
+          element={
+            <Home
+              isFetchingData={isFetchingData}
+              setIsFetchingData={setIsFetchingData}
+              displaySet={displaySet}
+            />
+          }
+        />
         <Route path="/pokemon/:id" element={<PokemonDetails />} />
       </Routes>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,52 +1,17 @@
 // <------------ React Components ------------>
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 // <------------ React Components ------------>
 import PokemonCard from "../components/PokemonCard";
 
-export default function Home() {
-  const [isFetchingData, setIsFetchingData] = useState(false);
-  const [dataSet, setDataSet] = useState([]);
-  const [displaySet, setDisplaySet] = useState([]);
-
-  // <------------ Get data from Firebase ------------>
-  const fetchDataSet = async (url) => {
-    setIsFetchingData(true);
-    console.log("Fetching Data");
-
-    try {
-      const res = await fetch(url);
-
-      if (res.status !== 200) {
-        throw new Error("Something went wrong.");
-      }
-
-      const data = await res.json();
-
-      setDataSet(data);
-      setDisplaySet(data.slice(1, 21));
-    } catch (error) {
-      console.log(error.message);
-    }
-
-    setIsFetchingData(false);
-    console.log("Finished Fetching Data");
-  };
-  // <------------ End of get data from Firebase ------------>
-
-  useEffect(() => {
-    const url =
-      "https://andrewtai-school-project-default-rtdb.asia-southeast1.firebasedatabase.app/.json";
-    fetchDataSet(url);
-  }, []);
-
+export default function Home(props) {
   // <------------ Home return view ------------>
   return (
     <>
-      {isFetchingData && <p>Fetching Data. Please be patient.</p>}
+      {props.isFetchingData && <p>Fetching Data. Please be patient.</p>}
       <h1>Pokédex</h1>
       <div className="display-area grid">
-        {displaySet.map((element) => {
+        {props.displaySet.map((element) => {
           return <PokemonCard {...element} key={Math.random()} />;
         })}
       </div>
