8024f5601856fac8397c454146d4980921e48132
Andrew Tai <andrewtai.ye@gmail.com>
2022-10-03 11:00:05 +0800

    Added React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+// <------------ React Components ------------>
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+// <------------ Components ------------>
+// eslint-disable-next-line no-unused-vars
 import PopulateDatabase from "./components/PopulateDatabase";
 import Splash from "./pages/Splash";
+import Home from "./pages/Home";
 
+// <------------ Styles ------------>
+import "./App.css";
+
+// <------------ Main App ------------>
 export default function App() {
   return (
     <div>
-      <Splash />
+      <Routes>
+        <Route path="/" element={<PopulateDatabase />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
